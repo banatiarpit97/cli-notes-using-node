30898616d0532846c6d40f951dbe00d4e61aecd6
arpit <banatiarpit97@yahoo.co.in>
2018-11-08 16:33:08 +0530

    arguements validation using yargs with help command

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const yargs = require('yargs');
 const notes = require('./notes');
-const argv = yargs.argv;
+titleOptions = {
+    describe: 'Title of note',
+    demand:true,
+    alias:'t'
+}
+bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title:titleOptions,
+        body:bodyOptions
+    })
+    .command('edit', 'Edit an existing note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('remove', 'Remove an existing note', {
+        title: titleOptions,
+    })
+    .command('show-note', 'Show an existing note', {
+        title: titleOptions,
+    })
+    .command('show-all-notes', 'Show all existing notes', {
+        title: titleOptions,
+    })
+    .help()
+    .argv;
 
 const command = argv['_'];
 if(!command.length){
@@ -8,59 +37,21 @@ if(!command.length){
 }
 else{
     if(command[0] == "add"){
-        if(validate(true, true)){
-            notes.add(argv.title, argv.body);
-        }
+        notes.add(argv.title, argv.body);
     }
     else if (command[0] == "remove") {
-        if (validate(true, false)) {
-            notes.remove(argv.title);
-        }
+        notes.remove(argv.title);
     }
     else if (command[0] == "edit") {
-        if (validate(true, true)) {
-            notes.edit(argv.title, argv.body);
-        }
+        notes.edit(argv.title, argv.body);
     }
     else if (command[0] == "show-note") {
-        if (validate(true, false)) {
-            notes.showNote(argv.title);
-        }
+        notes.showNote(argv.title);
     }
     else if (command[0] == "show-all-notes"){
-        if (validate(false, false)) {
-            notes.showAllNotes();
-        }
-    }
-    else if(command[0] == 'help-me'){
-        console.log('Welcome to CLI Notes App');
-        console.log('add(title, body) : To add a new note');
-        console.log('edit(title, body) : To edit an existing note');
-        console.log('remove(title) : To remove an existing note');
-        console.log('show-note(title) : To view an existing note');
-        console.log('show-all-notes() : To view all existing notes');
-        console.log('\n');
-        console.log('To enter title use --title="title-string"');
-        console.log('To enter body use --body="body-string"');
+        notes.showAllNotes();
     }
     else{
-        console.log("Not a valid command, use 'help-me' command to view all possible actions")
+        console.log("Not a valid command, use '--help' command to view all possible actions")
     }
-}
-
-function validate(title, body){
-    if(title){
-        if(!argv.title){
-            console.log("Enter title of note");
-            return false;
-        }
-    }
-
-    if (body) {
-        if (!argv.body) {
-            console.log("Enter body of note");
-            return false;
-        }
-    }
-    return true;
 }
